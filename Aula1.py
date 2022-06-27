fab0753c7c1e52872ebc6f79fde9481367e76415
Rodrigo Souza Silva <souza.rodrigo11@gmail.com>
2022-06-27 20:51:19 -0300

    Add files via upload

---
+++ Aula1.py
@@ -0,0 +1,8 @@
+Dia = input ('DIA = ' )
+Mes = input ('MES = ' )
+Ano = input ('Ano = ')
+print('Voçe Nasceu No Dia 25 de Fervereiro De 1998 . Correto?')
+############################ Desafio 3 ######################
+
+
+
