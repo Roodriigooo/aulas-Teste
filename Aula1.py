36861f156327a8235717f2dd8bd1826a045eef72
Rodrigo Souza Silva <souza.rodrigo11@gmail.com>
2022-06-28 13:28:07 -0300

    Update Aula1.py

--- Aula1.py
+++ Aula1.py
@@ -2,7 +2,6 @@ Dia = input ('DIA = ' )
 Mes = input ('MES = ' )
 Ano = input ('ANO = ')
 print('Voçe Nasceu No dia ' , Dia  , 'Do mes' , Mes , 'do ano' , Ano  )
-############################ Desafio 3 ######################
 
 
 
